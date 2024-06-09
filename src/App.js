c95b067d8fb4ebf4f7d5ca588f05f81f18093163
potate4 <sumaiyaahmed@iut-dhaka.edu>
2024-06-09 21:01:53 +0600

    final changes and the cloud version works

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import './App.css';
 import { Line, Bar } from 'react-chartjs-2';
 import {
     Chart as ChartJS,
     LineElement,
     CategoryScale,
+    BarController,
     LinearScale,
     PointElement,
     BarElement,
@@ -14,7 +15,17 @@ import {
 } from 'chart.js';
 import 'chartjs-adapter-date-fns';
 
-ChartJS.register(LineElement, CategoryScale, LinearScale, PointElement, BarElement, Tooltip, Legend, TimeScale);
+ChartJS.register(
+    LineElement,
+    CategoryScale,
+    BarController,
+    LinearScale,
+    PointElement,
+    BarElement,
+    Tooltip,
+    Legend,
+    TimeScale
+);
 
 const App = () => {
     const API_URL = 'https://sumaiyaahmed.pythonanywhere.com';
@@ -47,24 +58,7 @@ const App = () => {
     const limit = 10;
     const chartRef = useRef(null);
 
-    useEffect(() => {
-        fetchData();
-    }, [currentPage]);
-
-    useEffect(() => {
-        if (data.length > 0) {
-            setSelectedTradeCode(data[0].trade_code); // Ensure a trade code is selected initially
-        }
-    }, [data]);
-    
-
-    useEffect(() => {
-        if (selectedTradeCode) {
-            updateChartData();
-        }
-    }, [data, selectedTradeCode]);
-
-    const fetchData = () => {
+    const fetchData = useCallback(() => {
         fetch(`${API_URL}/data?page=${currentPage}&limit=${limit}`)
             .then(response => {
                 if (!response.ok) {
@@ -76,7 +70,67 @@ const App = () => {
                 setData(data);
             })
             .catch(error => console.error("Error fetching data:", error));
-    };
+    }, [API_URL, currentPage]);
+
+    const updateChartData = useCallback(() => {
+        const allDates = data.map(row => row.date);
+        const filteredData = data.filter(row => row.trade_code === selectedTradeCode);
+
+        const closePrices = allDates.map(date => {
+            const found = filteredData.find(row => row.date === date);
+            return found ? found.close : null;
+        });
+
+        const allClosePrices = data.map(row => row.close);
+        const volumes = data.map(row => row.volume);
+
+        setChartData({
+            labels: allDates,
+            datasets: [
+                {
+                    type: 'line',
+                    label: 'Close Price',
+                    data: closePrices,
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    yAxisID: 'y-axis-1',
+                },
+                {
+                    type: 'line',
+                    label: 'Close Price Unfiltered',
+                    data: allClosePrices,
+                    borderColor: 'rgba(175, 192, 192, 0.5)',
+                    backgroundColor: 'rgba(175, 192, 192, 0.2)',
+                    yAxisID: 'y-axis-1',
+                },
+                {
+                    type: 'bar',
+                    label: 'Volume',
+                    data: volumes,
+                    backgroundColor: 'rgba(153, 102, 255, 0.2)',
+                    borderColor: 'rgba(153, 102, 255, 1)',
+                    borderWidth: 1,
+                    yAxisID: 'y-axis-2',
+                }
+            ]
+        });
+    }, [data, selectedTradeCode]);
+
+    useEffect(() => {
+        fetchData();
+    }, [currentPage, fetchData]);
+
+    useEffect(() => {
+        if (data.length > 0) {
+            setSelectedTradeCode(data[0].trade_code); // Ensure a trade code is selected initially
+        }
+    }, [data]);
+
+    useEffect(() => {
+        if (selectedTradeCode) {
+            updateChartData();
+        }
+    }, [data, selectedTradeCode, updateChartData]);
 
     const updateRow = (id, newData) => {
         fetch(`${API_URL}/data/${id}`, {
@@ -110,51 +164,6 @@ const App = () => {
         setCurrentPage(prevPage => Math.max(prevPage - 1, 1));
     };
 
-    const updateChartData = () => {
-        const allDates = data.map(row => row.date);
-        const filteredData = data.filter(row => row.trade_code === selectedTradeCode);
-    
-        // Map each date to the corresponding close price or null if not present
-        const closePrices = allDates.map(date => {
-            const found = filteredData.find(row => row.date === date);
-            return found ? found.close : null;
-        });
-    
-        const allClosePrices = data.map(row => row.close);
-        const volumes = data.map(row => row.volume);
-    
-        setChartData({
-            labels: allDates,
-            datasets: [
-                {
-                    type: 'line',
-                    label: 'Close Price',
-                    data: closePrices,
-                    borderColor: 'rgba(75, 192, 192, 1)',
-                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                    yAxisID: 'y-axis-1',
-                },
-                {
-                    type: 'line',
-                    label: 'Close Price Unfiltered',
-                    data: allClosePrices,
-                    borderColor: 'rgba(175, 192, 192, 0.5)',
-                    backgroundColor: 'rgba(175, 192, 192, 0.2)',
-                    yAxisID: 'y-axis-1',
-                },
-                {
-                    type: 'bar',
-                    label: 'Volume',
-                    data: volumes,
-                    backgroundColor: 'rgba(153, 102, 255, 0.2)',
-                    borderColor: 'rgba(153, 102, 255, 1)',
-                    borderWidth: 1,
-                    yAxisID: 'y-axis-2',
-                }
-            ]
-        });
-    };
-    
     const handleTradeCodeChange = (event) => {
         setSelectedTradeCode(event.target.value);
     };
